f2e0dbbe946b178ef63ab5acd356c8e5b024b776
HarzemYldz <58432230+HarzemYldz@users.noreply.github.com>
2023-07-19 14:15:47 +0300

    Add files via upload

---
+++ OBS/src/Student.java
@@ -0,0 +1,69 @@
+public class Student {
+    String name, stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double average;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat, Course fizik, Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAverage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int matNote, int fizikNote, int kimyaNote) {
+        if (matNote >= 0 && matNote <= 100) {
+            this.mat.note = matNote;
+        }
+
+        if (fizikNote >= 0 && fizikNote <= 100) {
+            this.fizik.note = fizikNote;
+        }
+
+        if (kimyaNote >= 0 && kimyaNote <= 100) {
+            this.kimya.note = kimyaNote;
+        }
+
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.average);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAverage() {
+        this.average = (this.mat.note + this.fizik.note + this.kimya.note * this.kimya.verbalImpact) / (2.0 + this.kimya.verbalImpact);
+    }
+
+    public boolean isCheckPass() {
+        calcAverage();
+        return this.average > 55;
+    }
+
+    public void printNote() {
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+}
