994cf4ebb783140433205c7811e944711febaf10
HarzemYldz <58432230+HarzemYldz@users.noreply.github.com>
2023-07-19 10:33:07 +0300

    Add files via upload

---
+++ patikaOdev/src/patikaOdev/RecursiveUsAlma.java
@@ -0,0 +1,28 @@
+package patikaOdev;
+
+import java.util.Scanner;
+
+public class RecursiveUsAlma {
+
+    static int usAl(int taban, int us) {
+        if (us == 0) {
+            return 1;
+        } else {
+            return taban * usAl(taban, us - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Taban değerini giriniz: ");
+        int taban = scanner.nextInt();
+
+        System.out.print("Üs değerini giriniz: ");
+        int us = scanner.nextInt();
+
+        int sonuc = usAl(taban, us);
+        System.out.println("Sonuç: " + sonuc);
+    }
+}
+
